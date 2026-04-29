711d494d59536996f937697d4ea59aef60c74e46
Brandon <brandonmarella@gmail.com>
2026-04-29 17:17:03 -0400

    Cleaned up indentation

--- modules/features/home-manager.nix
+++ modules/features/home-manager.nix
@@ -32,22 +32,22 @@
       ];
 
       home.pointerCursor = {
-		gtk.enable = true;
-		package = pkgs.adwaita-icon-theme;
-		name = "Adwaita";
-		size = 24;
+        gtk.enable = true;
+        package = pkgs.adwaita-icon-theme;
+        name = "Adwaita";
+        size = 24;
       };
 
       programs.git = {
-		enable = true;
-		settings = {
-			user = {
-				name = "Brandon";
-				email = "brandonmarella@gmail.com";
-			};
-			init.defaultBranch = "main";
-			pull.rebase = false;
-		};
+        enable = true;
+        settings = {
+          user = {
+            name = "Brandon";
+            email = "brandonmarella@gmail.com";
+          };
+          init.defaultBranch = "main";
+          pull.rebase = false;
+        };
       };
 
       programs.kitty = {
@@ -56,77 +56,77 @@
       };
 
       programs.starship = {
-	  enable = true;
-	  settings = {
-	    add_newline = true;
-	    format = "[ ](bg:#3b4252 fg:#2e3440)[ ](bg:#434c5e fg:#3b4252)[ ](bg:#88c0d0 fg:#434c5e)[󱄅 ](bg:#88c0d0 fg:#434c5e)[ ](bg:#81a1c1 fg:#88c0d0)[$directory](bg:#81a1c1 fg:#000000)[ ](bg:#81a1c1 fg:#81a1c1)[ ](fg:#81a1c1)\n";
-	    directory = {
-	      style = "fg:#434c5e bg:#81a1c1";
-	      format = "[ $path ]($style)";
-	      truncation_length = 3;
-	      truncation_symbol = ".../";
-	    };
-	  };
-	};
+        enable = true;
+        settings = {
+          add_newline = true;
+          format = "[ ](bg:#3b4252 fg:#2e3440)[ ](bg:#434c5e fg:#3b4252)[ ](bg:#88c0d0 fg:#434c5e)[󱄅 ](bg:#88c0d0 fg:#434c5e)[ ](bg:#81a1c1 fg:#88c0d0)[$directory](bg:#81a1c1 fg:#000000)[ ](bg:#81a1c1 fg:#81a1c1)[ ](fg:#81a1c1)\n";
+          directory = {
+            style = "fg:#434c5e bg:#81a1c1";
+            format = "[ $path ]($style)";
+            truncation_length = 3;
+            truncation_symbol = ".../";
+          };
+        };
+      };
 
-	programs.neovim = {
-	  enable = true;
-	  defaultEditor = true;
-	  vimAlias = true;
-	  viAlias = true;
-    withRuby = false;
-    withPython3 = false;
-	};
+      programs.neovim = {
+        enable = true;
+        defaultEditor = true;
+        vimAlias = true;
+        viAlias = true;
+        withRuby = false;
+        withPython3 = false;
+      };
 
-	xdg.configFile."nvim" = {
-	  source = ./nvim;
-	  recursive = true;
-	};
+      xdg.configFile."nvim" = {
+        source = ./nvim;
+        recursive = true;
+      };
 
-  xdg.configFile."nvim/lazy-lock.json".enable = false;
+      xdg.configFile."nvim/lazy-lock.json".enable = false;
 
-	programs.tmux = {
-	  enable = true;
-	  prefix = "C-Space";
-	  baseIndex = 1;
-	  terminal = "tmux-256color";
-    mouse = true;
-	  plugins = with pkgs.tmuxPlugins; [
-	    nord
-	    vim-tmux-navigator
-	  ];
-	  extraConfig = ''
-	    bind-key P display-popup -w 80% -h 80% -E
-	    set -g pane-border-lines double
-      bind -r Left resize-pane -L 10
-      bind -r Right resize-pane -R 10
-      bind -r Up resize-pane -U 10
-      bind -r Down resize-pane -D 10
-      set -g repeat-time 700
-	  '';
-	};
+      programs.tmux = {
+        enable = true;
+        prefix = "C-Space";
+        baseIndex = 1;
+        terminal = "tmux-256color";
+        mouse = true;
+        plugins = with pkgs.tmuxPlugins; [
+          nord
+          vim-tmux-navigator
+        ];
+        extraConfig = ''
+          bind-key P display-popup -w 80% -h 80% -E
+          set -g pane-border-lines double
+          bind -r Left resize-pane -L 10
+          bind -r Right resize-pane -R 10
+          bind -r Up resize-pane -U 10
+          bind -r Down resize-pane -D 10
+          set -g repeat-time 700
+        '';
+      };
 
       programs.ssh = {
-		enable = true;
-		enableDefaultConfig = false; # This is deprecated, so setting to false now to get ahead
-		matchBlocks = {
-			"github.com" = {
-				identityFile = "~/.ssh/id_ed25519";
-				user = "git";
-			};
-			"*" = {
-				forwardAgent = false;
-				addKeysToAgent = "no";
-				compression = false;
-				serverAliveInterval = 0;
-				serverAliveCountMax = 3;
-				hashKnownHosts = false;
-				userKnownHostsFile = "~/.ssh/known_hosts";
-				controlMaster = "no";
-				controlPath = "~/.ssh/master-%r@%n:%p";
-				controlPersist = "no";
-			};
-		};
+        enable = true;
+        enableDefaultConfig = false; # This is deprecated, so setting to false now to get ahead
+        matchBlocks = {
+          "github.com" = {
+            identityFile = "~/.ssh/id_ed25519";
+            user = "git";
+          };
+          "*" = {
+            forwardAgent = false;
+            addKeysToAgent = "no";
+            compression = false;
+            serverAliveInterval = 0;
+            serverAliveCountMax = 3;
+            hashKnownHosts = false;
+            userKnownHostsFile = "~/.ssh/known_hosts";
+            controlMaster = "no";
+            controlPath = "~/.ssh/master-%r@%n:%p";
+            controlPersist = "no";
+          };
+        };
       };
 
       wayland.windowManager.hyprland = {
@@ -247,14 +247,14 @@
       };
 
       programs.bash = {
-		enable = true;
-		bashrcExtra = ''
-		    rebuild() {
-		      local host=''${1:-desktop}
-		      local extra_args=''${@:2}
-		      sudo nixos-rebuild switch --flake ~/nixos-config#$host $extra_args
-		    }
-		  '';
+        enable = true;
+        bashrcExtra = ''
+          rebuild() {
+            local host=''${1:-desktop}
+            local extra_args=''${@:2}
+            sudo nixos-rebuild switch --flake ~/nixos-config#$host $extra_args
+          }
+        '';
       };
 
       programs.hyprlock = {
