6841db94dc7fc9a8126947aac90b0400960fd160
Brandon <brandonmarella@gmail.com>
2026-05-12 23:56:21 -0400

    Updated naming of value

--- modules/hosts/work-laptop/configuration.nix
+++ modules/hosts/work-laptop/configuration.nix
@@ -94,7 +94,7 @@
 	  };
 
 	  # Enable touchpad support (enabled default in most desktopManager).
-	  services.xserver.libinput.enable = true;
+	  services.libinput.enable = true;
 
 	  users.users.brandon = {
 	    isNormalUser = true;
