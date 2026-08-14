b446446c6b95772c6c976aa96caeae445ead52db
Brandon <brandonmarella@gmail.com>
2026-08-14 12:32:18 -0400

    Fixes vterm issue in emacs
    Now launches the correct version of emacs instead of an unpacked version
    that I was accidentally using with the keybind

--- modules/features/home-manager.nix
+++ modules/features/home-manager.nix
@@ -117,7 +117,7 @@
             bind = [
               "$mod, C, exec, ${lib.getExe pkgs.kitty}"
               "$mod, B, exec, ${lib.getExe pkgs.firefox}"
-              "$mod, E, exec, ${lib.getExe pkgs.emacs}"
+              "$mod, E, exec, ${lib.getExe config.home-manager.users.brandon.programs.emacs.finalPackage}"
               "$mod, D, exec, ${lib.getExe pkgs.kitty} yazi"
 
               "$mod, Q, killactive"
